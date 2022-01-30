e85ec3fa0200ac67c76e6d4c278ce5f719ee3cae
KycKyc <cycypyc@gmail.com>
2022-01-30 19:14:58 +0300

    refactor: py script adjustmnets, convert all files to the same format

--- sync_to_en.py
+++ sync_to_en.py
@@ -2,7 +2,7 @@ import copy
 import json
 import os
 from collections import OrderedDict
-from pprint import pprint
+from pprint import pformat, pprint
 
 
 def sync_to_en():
@@ -31,21 +31,28 @@ def sync_to_en():
         return
 
     # Synchronize
-    # Load original locale
+    # Load en.json file
     with open(os.path.join('locales', locales['en'])) as _file:
         en_locale = json.JSONDecoder(object_pairs_hook=OrderedDict).decode(_file.read())
 
+    # Format en.json file
+    with open(os.path.join('locales', locales['en']), 'w', encoding="utf-8") as _file:
+        en_locale_dump = json.JSONEncoder(indent=2, ensure_ascii=False).encode(en_locale)
+        _file.write(en_locale_dump)
+
     for locale_file in locales['to_sync']:
         updated_locale_data = copy.deepcopy(en_locale)
-        missing_keys = []
+        unknown_keys = []
         with open(os.path.join('locales', locale_file)) as _file:
             for key, value in json.JSONDecoder(object_pairs_hook=OrderedDict).decode(_file.read()).items():
                 if key in en_locale:
                     updated_locale_data[key] = value
                 else:
-                    missing_keys.append(key)
+                    unknown_keys.append(key)
+            if len(unknown_keys) > 0:
+                print(f'Locale: {locale_file}, unknown keys detected:\n{pformat(unknown_keys)}')
 
-        locale_dump = json.JSONEncoder(indent=4, ensure_ascii=False).encode(updated_locale_data)
+        locale_dump = json.JSONEncoder(indent=2, ensure_ascii=False).encode(updated_locale_data)
         with open(os.path.join('locales', locale_file), 'w', encoding="utf-8") as _file:
             _file.write(locale_dump)
 
